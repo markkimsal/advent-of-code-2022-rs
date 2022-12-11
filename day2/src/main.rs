908f823c2083dcf071f109e67b5f732ba5b4cae2
Mark Kimsal <mark.kimsal@v1sports.com>
2022-12-11 14:10:00 -0500

    ft: part 2 day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,5 @@
 #![allow(non_snake_case)]
-use std::{fs, fmt::Display};
+use std::{fs};
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum GameToken {
@@ -20,12 +20,23 @@ struct Playbook {
 	 character: char,
 	 token: GameToken,
 }
-
+#[derive(Debug, PartialEq)]
+struct PlaybookPart2 {
+	 character: char,
+	 result: MatchResult,
+}
 struct Match {
 	playerA: GameToken,
 	playerB: GameToken,
 }
 
+#[derive(Debug, PartialEq)]
+struct HandRule {
+	playerA: GameToken,
+	playerB: GameToken,
+	player_a_result: MatchResult,
+	player_b_result: MatchResult,
+}
 impl Match {
 	fn scoreForPlayerA(&self) -> u8 {
 		0
@@ -76,7 +87,63 @@ impl Match {
 // impl Display for GameToken {
 // }
 fn main() {
-	let rules = vec![
+	let gameRules: Vec<HandRule> = vec![
+		HandRule {
+			playerA: GameToken::Paper,
+			playerB: GameToken::Paper,
+			player_a_result: MatchResult::Draw,
+			player_b_result: MatchResult::Draw,
+		},
+		HandRule {
+			playerA: GameToken::Paper,
+			playerB: GameToken::Rock,
+			player_a_result: MatchResult::Win,
+			player_b_result: MatchResult::Loss,
+		},
+		HandRule {
+			playerA: GameToken::Paper,
+			playerB: GameToken::Scissors,
+			player_a_result: MatchResult::Loss,
+			player_b_result: MatchResult::Win,
+		},
+		HandRule {
+			playerA: GameToken::Rock,
+			playerB: GameToken::Rock,
+			player_a_result: MatchResult::Draw,
+			player_b_result: MatchResult::Draw,
+		},
+		HandRule {
+			playerA: GameToken::Rock,
+			playerB: GameToken::Paper,
+			player_a_result: MatchResult::Loss,
+			player_b_result: MatchResult::Win,
+		},
+		HandRule {
+			playerA: GameToken::Rock,
+			playerB: GameToken::Scissors,
+			player_a_result: MatchResult::Win,
+			player_b_result: MatchResult::Loss,
+		},
+		HandRule {
+			playerA: GameToken::Scissors,
+			playerB: GameToken::Rock,
+			player_a_result: MatchResult::Loss,
+			player_b_result: MatchResult::Win,
+		},
+		HandRule {
+			playerA: GameToken::Scissors,
+			playerB: GameToken::Paper,
+			player_a_result: MatchResult::Win,
+			player_b_result: MatchResult::Loss,
+		},
+		HandRule {
+			playerA: GameToken::Scissors,
+			playerB: GameToken::Scissors,
+			player_a_result: MatchResult::Draw,
+			player_b_result: MatchResult::Draw,
+		},
+	];
+	let inputTokens = vec![
 		Playbook {
 			character: 'A',
 			token: GameToken::Rock,
@@ -102,25 +169,48 @@ fn main() {
 			token: GameToken::Scissors,
 		},
 	];
-	dbg!(GameToken::Rock == GameToken::Scissors);
+	let inputPart2 = vec![
+		PlaybookPart2 {
+			character: 'X',
+			result: MatchResult::Loss,
+		},
+		PlaybookPart2 {
+			character: 'Y',
+			result: MatchResult::Draw,
+		},
+		PlaybookPart2 {
+			character: 'Z',
+			result: MatchResult::Win,
+		},
+	];
+	let part2 = true;
 
 	let input_part_1 = fs::read_to_string("./fixtures/input_part_1.txt")
 		.expect("could not parse input_part_1.txt from fixtures/ dir.");
 
-    let matches = parseMactches(&input_part_1, &rules);
 	let mut player_b_score: u32 = 0;
-	for mach in matches {
-		let result = mach.playerAResult();
-		dbg!(" match: ", mach.playerA, mach.playerB);
-		dbg!(result);
-		player_b_score += mach.playerBScore();
+	if (part2) {
+		let matches = parseMactchesPart2(&input_part_1, &inputTokens, &&inputPart2, &gameRules);
+		for mach in matches {
+			let result = mach.playerAResult();
+			//dbg!(" match: ", mach.playerA, mach.playerB);
+			player_b_score += mach.playerBScore();
+		}
+	} else {
+		let matches = parseMactches(&input_part_1, &inputTokens);
+		for mach in matches {
+			let result = mach.playerAResult();
+			//dbg!(" match: ", mach.playerA, mach.playerB);
+			dbg!(result);
+			player_b_score += mach.playerBScore();
+		}
 	}
 	println!("Player B (you)'s final score: {}", player_b_score);
 }
 
 // take a list of newline separated ABC space XYZ
 // values and construct matches with them.
-fn parseMactches(input: &str, rules: &Vec<Playbook>) -> Vec<Match> {
+fn parseMactches(input: &str, inputTokens: &Vec<Playbook>) -> Vec<Match> {
 
     let mut matches: Vec<Match> = Vec::new();
 
@@ -132,12 +222,58 @@ fn parseMactches(input: &str, rules: &Vec<Playbook>) -> Vec<Match> {
 		let mut splitChar = letters.split(' ');
 		let playerAChoice = splitChar.next().expect("Input matches format not correct").chars().next().expect("Input matches format not correct");
 		let playerBChoice = splitChar.next().expect("Input matches format not correct").chars().next().expect("Input matches format not correct");
-		let tokenA:Vec<&Playbook> = rules.into_iter().filter(|p| -> bool { p.character == playerAChoice }).collect();
-		let tokenb:Vec<&Playbook> = rules.into_iter().filter(|p| -> bool { p.character == playerBChoice }).collect();
+		let tokenA:Vec<&Playbook> = inputTokens.into_iter().filter(|p| -> bool { p.character == playerAChoice }).collect();
+		let tokenb:Vec<&Playbook> = inputTokens.into_iter().filter(|p| -> bool { p.character == playerBChoice }).collect();
 		// dbg!(&tokenA.first().unwrap().token, &tokenb.first().unwrap().token);
 		matches.push(
 			//Match {playerA: GameToken::Rock, playerB: GameToken::Scissors}
 			Match {playerA: tokenA.first().unwrap().token, playerB: tokenb.first().unwrap().token}
+		);
+	}
+
+    matches
+}
+
+// take a list of newline separated ABC space XYZ
+// values and construct matches with them.
+// use inputPart2 to determin what game token to play
+fn parseMactchesPart2(input: &str, inputTokens: &Vec<Playbook>, cribSheet: &Vec<PlaybookPart2>, gameRules: &Vec<HandRule>) -> Vec<Match> {
+
+    let mut matches: Vec<Match> = Vec::new();
+
+    let v: Vec<&str> =  input.split('\n').collect::<Vec<_>>();
+	for letters in v {
+		if letters.len() == 0 {
+			break
+		}
+		let mut splitChar = letters.split(' ');
+		let playerAChoice = splitChar.next().expect("Input matches format not correct").chars().next().expect("Input matches format not correct");
+		let playerBChoice = splitChar.next().expect("Input matches format not correct").chars().next().expect("Input matches format not correct");
+
+		let tokenA:Vec<&Playbook> = inputTokens.into_iter().filter(|p| -> bool { p.character == playerAChoice }).collect();
+		let playerAToken = tokenA.first().unwrap().token;
+
+		let desiredResult:MatchResult = match playerBChoice {
+			'X' => MatchResult::Loss,
+			'Y' => MatchResult::Draw,
+			'Z' => MatchResult::Win,
+			_ => MatchResult::Draw,
+		};
+
+		let tokenB:Vec<&HandRule> = gameRules.into_iter().filter(|hr| -> bool { hr.player_b_result == desiredResult && hr.playerA == playerAToken}).collect();
+		//cribSheet.into_iter().filter(|p| -> bool { p.character == playerBChoice }).collect();
+
+		dbg!(
+			playerBChoice,
+			playerAChoice,
+			&tokenA.first().unwrap().token,
+			desiredResult,
+			&tokenB.first().unwrap().playerB,
+		);
+		// dbg!(&tokenA.first().unwrap().token, &tokenb.first().unwrap().token);
+		matches.push(
+			//Match {playerA: GameToken::Rock, playerB: GameToken::Scissors}
+			Match {playerA: tokenA.first().unwrap().token, playerB: tokenB.first().unwrap().playerB}
 		);
 	}
 
